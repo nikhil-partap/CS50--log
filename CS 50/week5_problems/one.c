c86c633c1872c146ee9372a0c033d070ae2cad3b
nikhil-partap <nikhilpartapsingh88@gmail.com>
2025-07-09 17:22:49 +0530

    update

---
+++ CS 50/week5_problems/one.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<cs50.h>
+
+typedef struct node {
+    int value;
+    struct node *next;
+} node;
+char list[50];
+int main (void){
+    
+
+    for (int i = 0; i < 5; i++)
+    {
+    node *one = malloc(sizeof(node));
+
+    
+    node one = {1, NULL};
+    
+    list = &;
+    }
+    
+
+
+
+}
