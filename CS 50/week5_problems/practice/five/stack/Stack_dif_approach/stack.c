e635624e6ff53a8e768b715027f2ffeef6d29e08
nikhil-partap <nikhilpartapsingh88@gmail.com>
2025-07-13 18:14:25 +0530

    update

--- CS 50/week5_problems/practice/five/stack/Stack_dif_approach/stack.c
+++ CS 50/week5_problems/practice/five/stack/Stack_dif_approach/stack.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
 #include "stack.h"
+// structure of each stack box
 typedef struct node{
     int data;
     struct stack *next; 
@@ -23,9 +23,30 @@ void push(int value){
 bool pop(int *popped_value){
     if(is_empty())return false;
 
-    
+    *popped_value = top->data;
+    Node *temp = top;
+    top = top->next;
+    free(temp);
+    return true;
+}
+bool is_empty(void){
+    return !top;
 }
 
+void free_stack (void){
+    while(is_empty){
+        int temp; 
+        pop(&temp);
+    }
+}
 
+bool peek(int *top_value){
+    if(is_empty){
+        fprintf(stderr, "The stack is already empty ");
+        return false;
+    }
+    *top_value = top->data;
+    return true;
+}
 
 
---
+++ CS 50/week5_problems/practice/five/stack/Stack_dif_approach/stack.h
@@ -0,0 +1,20 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include<stdbool.h>
+
+//this add a new_node to the stack and store the int(value) in new_node->data 
+void push(int value);
+
+// this free the top node and update the top to point at the next node 
+bool pop(int *popped_value);
+
+// check if the top points to NULL or if the stack is empty
+bool is_empty(void);
+
+// it free all the node in the stack starting from the top node with the help of pop function 
+void free_stack (void);
+
+// it takes a look at the data stored in the top node 
+bool peek(int *top_value);
+
