2181436386e3709bce451ff911e769cbd58c7856
nikhil-partap <nikhilpartapsingh88@gmail.com>
2025-07-13 15:45:14 +0530

    update

---
+++ CS 50/week5_problems/practice/five/stack/Stack_dif_approach/stack.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "stack.h"
+typedef struct stack{
+    int data;
+    struct stack *next; 
+} stack;
+
+
+
+// push , pop , is_empty , free_stack , peek
+
+int main(void){
+
+}
+
+
+
