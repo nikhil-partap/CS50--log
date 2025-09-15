c1153a45e387897b478df22b2a280017e7a9455c
nikhil-partap <nikhilpartapsingh88@gmail.com>
2025-09-15 17:30:53 +0530

    Refactor cash change calculator: encapsulate logic in a function, improve input handling, and enhance readability

--- 6thWeekProblems/cash,credit/cash/cash.py
+++ 6thWeekProblems/cash,credit/cash/cash.py
@@ -1,23 +1,40 @@
-change = float(input("Change: "))
+def greed (Amount):
+    # print(change)
+    quater_change = (cents//25)
+    dime_left = cents%25
+    # print(quater_change)
 
-# then check the input accross 1 5 10 25
-
-quater_change = (change//0.25)
-print(quater_change)
-
-no_of_dime = change -quater_change
-dime_change = ((no_of_dime)//0.10)
-print(dime_change)
-
-
-no_of_nickel= no_of_dime - dime_change
-nickel_change = ((no_of_nickel)//0.05)
-print(nickel_change)
+    # no_of_dime = change -quater_change
+    dime_change = (dime_left//10)
+    nickel_left = dime_left%10
+    # print(dime_change)
+    # print(nickel_left)
 
 
-no_of_pennie = no_of_nickel- nickel_change
-pennie_change = int(no_of_pennie/0.01)
-print(no_of_pennie)
+    # no_of_nickel= no_of_dime - dime_change
+    nickel_change = (nickel_left//5)
+    pennie_left = nickel_left%5
+    # print(nickel_change)
 
 
-print(quater_change+dime_change+nickel_change+no_of_pennie)
+    # no_of_pennie = no_of_nickel- nickel_change
+    pennie_change = int(pennie_left)
+    # print(pennie_change)
+
+    total_coins = quater_change+dime_change+nickel_change+pennie_left
+
+    print(total_coins)
+
+while True:
+    try:
+        change = float(input("Change: "))
+        cents = int(change*100)
+
+        
+        if cents >= 0:
+            greed(cents)
+            break
+        
+
+    except ValueError :
+        pass
